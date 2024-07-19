53a85ed4299d1cb1898fbef8a0db3ab7930422a2
Bagul Sneha <shravanbagul143@gmail.com>
2024-07-19 08:11:51 -0500

    Add files via upload

---
+++ pr1.2/1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf(" -------\n");
+	printf("|       |\n");
+	printf("|       |\n");
+	printf("| SNEHA |\n");
+	printf("|       |\n");
+	printf("|       |\n");
+	printf(" -------\n");
+}
---
+++ pr1.2/2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf(" -------\n");
+	printf("|       |\n");
+	printf("S       |\n");
+	printf("N       |\n");
+	printf("E       |\n");
+	printf("H       |\n");
+	printf("A       |\n");
+	printf("|       |\n");
+	printf(" -------\n");
+}
