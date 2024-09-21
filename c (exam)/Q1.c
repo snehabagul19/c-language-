807215f14fc6d60d17a8d0ae9ff3420b3ca2395b
Bagul Sneha <shravanbagul143@gmail.com>
2024-09-21 09:32:05 -0500

    Add files via upload

---
+++ c (exam)/Q1.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2;
+	
+	printf("Enter num 1 : ");
+	scanf("%d",&num1);
+	printf("Enter num 2 : ");
+	scanf("%d",&num2);
+	
+	if (num1<num2)
+	{ 
+	    printf("The num1 is minimum");	
+ }
+ else  
+ {
+ 	printf("num 2 is minimum");
+ }
+}
